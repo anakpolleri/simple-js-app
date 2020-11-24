b320b6b1f7e63b8ced963bef51afec37d09486a3
Ana <anakpolleri@gmail.com>
2020-11-24 13:11:43 -0800

    JS Exercise 1.1

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+ alert('Hello Everyone');
+let myName='Ana';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let favoriteFood='ice cream';
+document.write(favoriteFood);
