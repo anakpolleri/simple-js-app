7997be0c0bc0a6d62ecce7a02c87c54c75a820a0
Ana <anakpolleri@gmail.com>
2020-12-08 11:38:29 -0800

    updated for loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,41 @@
-// IIFY //
-(function (){
-  let data = {};
-
-  function add(pokemon){
-    pokemonList.push(pokemon);
-    }
-    function getAll (){
-      return pokemonList;
-    }
-  }
-
 let pokemonRepository = (function() {
-let pokemonList = [
-  {
-    name: "Charmandor",
-    height: 2.00,
-    type: ['fire'],
-  },
-  {
-    name: "Jigglypuff",
-    height: 1.08,
-    type: ['normal','fairy'],
-  },
-  {
-    name: "Slowbro",
-    height: 5.03,
-    type: ['water','psychic'],
+  let pokemonList = [
+    {
+      name: "Charmandor",
+     height: 2.00,
+     type: ['fire'],
+   },
+   {
+     name: "Jigglypuff",
+     height: 1.08,
+     type: ['normal','fairy'],
+   },
+   {
+     name: "Slowbro",
+     height: 5.03,
+      type: ['water','psychic'],
+    }
+  ];
+// for loop 
+for (let i=0;i < pokemonList.length; i++){
+  if (pokemonList[i].height <5.03 && pokemonList[i].height >2.00){
+  console.log(pokemonList[i].name + "is average");
+} else if (pokemonList[i].height  <5.03){
+  console.log(pokemonList[i].name + "is small");
+}
+}
+  function add(pokemon) {
+  pokemonList.push(pokemon);
   }
-];
-// for loop //
-let pokemonListNames = ['Charmandor', 'Jigglypuff', 'Slowbro'];
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+  })();
+  pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.name + "- type: " + pokemon.type + "-" + " (height " + pokemon.height + ")" + "<br>");
+  });
 
-for (let i = 0; i < pokemonListNames.length; i++){
-  console.log(pokemonListNames[i]);
-}
-// for each loop //
-
-pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + 'is' + pokemon.height + 'inches tall');
-});
-
-
-function add(pokemon) {
-pokemonList.push(pokemon);
-}
-function getAll() {
-  return pokemonList;
-}
-return {
-  add: add,
-  getAll: getAll,
-};
-})();
-pokemonRepository.getAll().forEach(function(pokemon){
-  document.write(pokemon.name + "- type: " + pokemon.type + "-" + " (height " + pokemon.height + ")" + "<br>");
-});
-})();
