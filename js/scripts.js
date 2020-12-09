485b1f268f1aaee15a4887690408098630ec643a
Ana <anakpolleri@gmail.com>
2020-12-08 20:55:25 -0800

    Updated 1.5 IIFE and ForEach Loop
    - Created functions within IIFE
    - Added new pokemon to original list
    - Organized code and added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-let pokemonRepository = (function() {
   let pokemonList = [
     {
       name: "Charmandor",
@@ -14,28 +13,52 @@ let pokemonRepository = (function() {
      name: "Slowbro",
      height: 5.03,
       type: ['water','psychic'],
-    }
+    },
   ];
-// for loop 
-for (let i=0;i < pokemonList.length; i++){
-  if (pokemonList[i].height <5.03 && pokemonList[i].height >2.00){
-  console.log(pokemonList[i].name + "is average");
-} else if (pokemonList[i].height  <5.03){
-  console.log(pokemonList[i].name + "is small");
-}
-}
-  function add(pokemon) {
-  pokemonList.push(pokemon);
+
+  let pokemonRepository = (function () {
+    let list = [];
+
+   function add(pokemon) {
+     list.push(pokemon);
+   }  
+  // for each loop
+  function myPokeList(pokemon) {
+    console.log(pokemon.name + " has a height of: " + pokemon.height + " and type of: " + pokemon.type);
   }
   function getAll() {
-    return pokemonList;
+  return list; 
   }
-  return {
-    add: add,
-    getAll: getAll,
-  };
+    return {
+      add: add,
+      myPokeList: myPokeList,
+      getAll: getAll,
+    };
   })();
-  pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + "- type: " + pokemon.type + "-" + " (height " + pokemon.height + ")" + "<br>");
-  });
+// add external pokemonList to IIFE pokemonRepository
+pokemonList.forEach(function (pokemon) {
+  pokemonRepository.add(pokemon);
+});
+
+// create new variable and assign value of pokemonRepository
+let allPokemon = pokemonRepository.getAll();
+
+// display list
+console.log("my initial pokemon list: ", allPokemon);
+
+// create new pokemon item
+let newPokemon = {
+  name: "Kirlia",
+  height: 2.07,
+  type: ["psychic", "fairy"],
+};
+//add new pokemon to list
+pokemonRepository.add(newPokemon);
+
+console.log("my new pokemon list: ", allPokemon);
+//print list with item details with forEach function
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.myPokeList(pokemon);
+  document.write(pokemon.name + "- type: " + pokemon.type + "-" + " (height " + pokemon.height + ")" + "<br>");
+});
 
